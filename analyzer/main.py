984bf113c9bb821797b0319c6fffbfe5b5eb2f92
Clément GHYS <clement.cg15@gmail.com>
2023-12-10 15:36:20 +0100

    test  logger

--- analyzer/main.py
+++ analyzer/main.py
@@ -9,9 +9,11 @@ from segment import segment
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG,format='%(asctime)s %(message)s')
+    logging.debug('This message should go to the log file')
     print (check_available_weights())
     img_path = 'assets/61.jpg'
     text_prompt = ['cap', 'shirt', 'sunglasses', 'shoe', 'sock', 'backpack', 'sticks', 'bib', 'trousers']
     annotated_image, detections, phrases = detect_with_dino(img_path, text_prompt)
-    print (detections)
+    logging.warning("test")
     segmented_image, detections = segment(detections, img_path)
