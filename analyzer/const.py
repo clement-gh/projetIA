6db862c7d6ac51a41d5851801cbaaff36b6032d8
Clément GHYS <clement.cg15@gmail.com>
2023-12-10 15:59:49 +0100

    Update const.py

--- analyzer/const.py
+++ analyzer/const.py
@@ -2,19 +2,40 @@
 
 import torch
 import os
+import logging
 
+# WEIGHTS FOLDER GLOBAL
 WEIGTHS_FOLDER = "H:\IA_project\Poids"
+
+# WEIGHTS PATH FOR SAM
+# SAM
 SAM_WEIGHTS_PATH = WEIGTHS_FOLDER + "/sam_vit_h_4b8939.pth"
 SAM_ENCODER_VERSION = "vit_h"
 
+#SAM HQ
 SAM_HQ_WEIGHTS_PATH = WEIGTHS_FOLDER + "/sam_hq_vit_l.pth"
 SAM_HQ_ENCODER_VERSION = "vit_l"
 
+# WEIGHTS PATH FOR GROUNDING DINO
 GROUNDING_DINO_T_CHECKPOINT_PATH = WEIGTHS_FOLDER + "/groundingdino_swint_ogc.pth"
 GROUNDING_DINO_B_CHECKPOINT_PATH = WEIGTHS_FOLDER + "/groundingdino_swint_ogc.pth"
-GROUNDING_DINO_CONFIG_PATH ="./GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
+GROUNDING_DINO_CONFIG_PATH_FOR_T ="./GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
+GROUNDING_DINO_CONFIG_PATH_FOR_B ="./GroundingDINO/groundingdino/config/GroundingDINO_SwinB_COGCOOR.py"
+
+
+
+URL_DINO_WEIGHTS_T = "https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha/groundingdino_swint_ogc.pth"
+URL_DINO_WEIGHTS_B = "https://github.com/IDEA-Research/GroundingDINO/releases/download/v0.1.0-alpha2/groundingdino_swinb_cogcoor.pth"
+URL_SAM_WEIGHTS = "https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth"
+URL_SAM_WEIGHTS_HQ = "https://drive.google.com/file/d/1Uk17tDKX1YAKas5knI4y9ZJCo0lRVL0G/view"
+#https://drive.google.com/u/0/uc?id=1Uk17tDKX1YAKas5knI4y9ZJCo0lRVL0G&export=download
+
+URL_YOLO_CUSTOM_WEIGHTS ="https://github.com/clement-gh/4A-Internship/raw/main/best.pt"
+URL_YOLO_WEIGHTS="https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt"
+
 
 DEVICE=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 
 ### TODO: use this function befor any call to the models !!
