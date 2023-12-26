e1102ca014ee5590c7aea7589843dd8c620c90a3
Clément <clement.cg15@gmail.com>
2023-12-26 15:03:17 +0100

    upload componant

--- front-app/src/app/app.module.ts
+++ front-app/src/app/app.module.ts
@@ -8,7 +8,17 @@ import { SearchComponent } from './pages/search/search.component';
 import { UploadComponent } from './pages/upload/upload.component';
 import { ConnexionComponent } from './pages/connexion/connexion.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { UploadDirective } from './directives/upload.directive';
 
+
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
+import { DragAndDropComponent } from './components/drag-and-drop/drag-and-drop.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +27,21 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     SearchComponent,
     UploadComponent,
     ConnexionComponent,
-    NavbarComponent
+    NavbarComponent,
+    UploadDirective,
+    DragAndDropComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatSnackBarModule
+
   ],
   providers: [
     provideClientHydration()
